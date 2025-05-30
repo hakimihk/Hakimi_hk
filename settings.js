6857b066ace38b785439cf9d72a49416e5f22468
hakimihk <efootballhakimi2@gmail.com>
2025-05-30 07:08:54 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: 'Bot',
   botName: "Group AI Bot",
   botOwner: 'Hakimi hk', // Your name
-  ownerNumber: '+252611533130', //Your number
+  ownerNumber: '+252621121089', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
